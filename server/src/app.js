9007fb7c179624aa659c9d56a91e8f19392a3fd5
payyer <quocanhle112@gmail.com>
2025-04-13 20:45:33 +0700

    be/feat/ create connect to mongodb and wirte funciton check over load

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
+const db = require("./db/init.mongodb");
 const express = require("express");
 const morgan = require("morgan");
 const compression = require("compression");
+const { checkOverLoad } = require("./helpers/check.connect");
 const helmet = require("helmet");
 const app = express();
 
@@ -10,7 +13,8 @@ app.use(helmet());
 app.use(compression()); // compress respone reduce size
 
 // init database
-
+db.getinstance();
+checkOverLoad();
 // init routes
 
 // handling error
